38693a5ed872daeed8ae3451cd23aafcbf83a4e2
Zenith-hasan <abuhasan323382@gmail.com>
2025-05-21 20:31:03 +0600

    basic discussion on numbers

---
+++ 02-Basic/07-Number.js
@@ -0,0 +1,41 @@
+// lets talk about the Numbers in JavaScript
+
+// Number Constructor
+
+const val = new Number(33);
+console.log(val);
+
+const num = 44;
+console.log(typeof num);
+
+// EPSILON - represent the difference between 1 and the smallest floating point number greater than 1.
+
+console.log(Number.EPSILON);
+
+// MAX_SAFE_INTEGER - returns the maximum safe integer value
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+// MAX_VALUE
+
+console.log(Number.MAX_VALUE);
+
+// MIN_VALUE - smallest possible value a number in JavaScript can have
+
+console.log(Number.MIN_VALUE);
+
+// MIN_SAFE_INTEGER
+
+console.log(Number.MIN_SAFE_INTEGER);
+
+// Number.Nan returns NaN
+
+console.log(Number.NaN);
+
+// NEGATIVE_INFINITY - A value that is less than MIN_VALUE
+
+console.log(Number.NEGATIVE_INFINITY)
+
+// POSITIVE_INFINITY
+
+console.log(Number.POSITIVE_INFINITY)
