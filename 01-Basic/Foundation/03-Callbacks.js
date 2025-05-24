f9b18635e99f14e306640e8f3e5d427f52d8f1e1
Zenith-hasan <abuhasan323382@gmail.com>
2025-05-24 19:00:07 +0600

    callbacks basic discussion

--- 01-Basic/Foundation/03-Callbacks.js
+++ 01-Basic/Foundation/03-Callbacks.js
@@ -1,4 +1,44 @@
 // let's know about the callbaks in JavaScript
 
+// A callback is a function passed as an argument to another function , to be invoked later.
+
+// A simple example of the callbacks function
+
+function greet(name, callback){
+	console.log("Hello, " + name)
+	callback();
+}
+
+function sayBye(){
+	console.log("GoodBye!")
+}
+
+greet("Alix",sayBye)
+
+//! Usecase of the callback function : 
+
+	// Asyncronous operation 
+	// Custom behaviour after an operation completes
+	// Making functions more flexible and reusable
+
+//! Types of callback is Syncronous and Asyncronous Callbacks.
+
+// Example of Syncronous callbacks - called immdediately, during execution of the parent function.
+
+// const arr = [1,2,3]
+// arr.forEach(function (item) {
+//   console.log(item);
+// });
+
+// Asynchronous Callback - Executed later, e.g., after an I/O task completes.
+
+setTimeout(() => {
+	console.log(`Executed after 2 seconds`)
+
+},2000)
+
+
+
+// Callback function
 
 
