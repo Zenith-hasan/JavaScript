b2e0d032a80abb54e99ec2b15eb73a9f895cc165
Zenith-hasan <abuhasan323382@gmail.com>
2025-05-25 23:28:38 +0600

    basic discussion on OOP in JS and Date as well

---
+++ 02-Basic/Date/02-Date.js
@@ -0,0 +1,20 @@
+// creating a function that can determine how much time it takes to execute a particular function  
+
+function calculateSum(){
+	let a = 0;
+	for(let i=0; i<1000000088; i++){
+		a = a + 1
+
+	}
+	return a;
+}
+
+const beforeExcu = new Date();
+const beforeTimeInMs = beforeExcu.getTime();
+calculateSum();
+
+const afterExcu = new Date();
+const afterTimeInMs = afterExcu.getTime();
+
+const funcRunTime = afterTimeInMs - beforeTimeInMs;
+console.log(funcRunTime)
--- 02-Basic/OOP JS/01-Classes.js
+++ 02-Basic/OOP JS/01-Classes.js
@@ -1,15 +1,21 @@
 class Animal {
-	constructor(name, legCount, color ){
-		this.name = name;
-		this.legCount = legCount;
-		this.color = color;
-	}
+  constructor(name, legCount, color) {
+    this.name = name;
+    this.legCount = legCount;
+    this.color = color;
+  }
 
-	describe(){
-		return `${this.name} is ${this.color} in color and it has ${this.legCount} legs.`
-	}
+  describe() {
+    return `${this.name} is ${this.color} in color and it has ${this.legCount} legs.`;
+  }
+
+  // static method
+  static hello() {
+    console.log(`Thanks for Visiting Our Zoo :)`);
+  }
 }
 
-let animal01 = new Animal();
-animal01()
-describe(animal01) 
+let animal01 = new Animal("Zebra", "white-black", "4");
+
+console.log(animal01.describe());
+Animal.hello();
